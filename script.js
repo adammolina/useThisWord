71d6786ea12116112f270ca2af48c3bb6bb3a9cf
Lukas Mo <67667042+adammolina@users.noreply.github.com>
2020-10-11 08:49:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function checkWord() {
+    var input = document.getElementById("userInput");
+    var badWords = ["fuck", "ass", "shit", "crap", "bloody", "git", "bullshit", "pissed", "bitch", "tit", "prick" "asshole", "twat", "pussy", "punani", "cock", "dick", "dickhead", "bastard", "motherfucker", "wanker", "cunt"]
+
+    for (i = 0; i<badWords.length, i++) {
+      prompt("badWords");
+    }
+
+
+
+  }
