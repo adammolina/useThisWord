ae8cfac526ac87a1c29fdfdadf2aa39d43a4e01d
Lukas Mo <67667042+adammolina@users.noreply.github.com>
2020-10-11 08:58:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function checkWord() {
     var input = document.getElementById("userInput");
     var badWords = ["fuck", "ass", "shit", "crap", "bloody", "git", "bullshit", "pissed", "bitch", "tit", "prick" "asshole", "twat", "pussy", "punani", "cock", "dick", "dickhead", "bastard", "motherfucker", "wanker", "cunt"]
 
-    for (i = 0; i<badWords.length, i++) {
-      prompt("badWords");
+    if (input.includes(badWords)) {
+      alert("No can do.")
+    } else {
+      alert("You're good to go!")
     }
 
 
