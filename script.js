bded38a17ee2a566b734560736f528b57b72fdf9
Lukas Mo <67667042+adammolina@users.noreply.github.com>
2020-12-08 21:51:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ myForm.addEventListener("submit", (e) => {
   function theBadWord() {
     var userInput = document.getElementById("theWord").value;
     var realWord = userInput.toLowerCase();
-    var badWordArr = ["fuck", "shit", "bitch", "cunt", "ass", "dick", "dickhead", "pussy", "wanker", "fucker", "motherfucker", "dogshit", "asshole", "balls", "bullshit", "tits", "tit", "prick", "twat", "punani", "minge", "cock", "bastard", "nig", "nigga", "nigger", "spic", "fag", "faggot","horseshit"];
+    var badWordArr = ["fuck", "shit", "bitch", "cunt", "ass", "dick","dumbass" "dickhead", "pussy", "wanker", "fucker", "motherfucker", "dogshit", "asshole", "balls", "bullshit", "tits", "tit", "prick", "twat", "punani", "minge", "cock", "bastard", "nig", "nigga", "nigger", "spic", "fag", "faggot","horseshit"];
 
     if (badWordArr.includes(realWord)) {
       document.querySelector("h2").innerHTML = "You can't say that at work. <br><br>Dumbass.";
